4ab1a723d773a7f380f5e4f824d40df7f9ef7f8b
Stefan Natchev <snatchev@twitter.com>
2015-12-15 15:17:22 -0500

    use bobthefish powerline-style prompt

---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,12 @@
+# You can override some default title options in your config.fish:
+#     set -g theme_date_format "+%a %H:%M"
+
+function fish_right_prompt -d 'bobthefish is all about the right prompt'
+  set_color $fish_color_autosuggestion[1]
+  set -q theme_date_format; or set -l theme_date_format "+%c"
+  #date $theme_date_format
+  whoami
+  echo -n '@'
+  hostname
+  set_color normal
+end
